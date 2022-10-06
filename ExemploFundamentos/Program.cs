024f7d30c69184b1fa21653b274d654aa817a674
Ramon Palheta Pessoa <ramon.palheta.pessoa@hotmail.com>
2022-10-06 18:14:29 -0400

    Utilizando o DateTime

--- ExemploFundamentos/Program.cs
+++ ExemploFundamentos/Program.cs
@@ -2,19 +2,27 @@
 using ExemploFundamentos.Models;
 
 
-string apresentacao = "Hello, welcome";
+DateTime dataAtual = DateTime.Now;
 
-int quantidade = 3;
- double altura = 1.84;
+System.Console.WriteLine(dataAtual);
 
- decimal preco = 1.50M;
- bool condicao = true;
+DateTime dataAtua2 = DateTime.Now.AddDays(2);
+System.Console.WriteLine(dataAtua2);
+System.Console.WriteLine(dataAtua2.ToString("dd/MM/yyyy HH:mm"));
 
- Console.WriteLine( apresentacao);
- Console.WriteLine("Quantidade:" + quantidade);
- Console.WriteLine("Altura: " + altura.ToString("0.00"));
- Console.WriteLine("Preço: " + preco);
- Console.WriteLine("Condição: " + condicao);
+// string apresentacao = "Hello, welcome";
+
+// int quantidade = 3;
+//  double altura = 1.84;
+
+//  decimal preco = 1.50M;
+//  bool condicao = true;
+
+//  Console.WriteLine( apresentacao);
+//  Console.WriteLine("Quantidade:" + quantidade);
+//  Console.WriteLine("Altura: " + altura.ToString("0.00"));
+//  Console.WriteLine("Preço: " + preco);
+//  Console.WriteLine("Condição: " + condicao);
 
 
 
