c50c5027f1cfe2ec3220f8fecab6aefbf1dac0a7
Ramon Palheta Pessoa <ramon.palheta.pessoa@hotmail.com>
2022-10-10 16:43:34 -0400

    Operadores e combinando operadores

--- ExemploFundamentos/Program.cs
+++ ExemploFundamentos/Program.cs
@@ -2,13 +2,22 @@
 using ExemploFundamentos.Models;
 
 
-DateTime dataAtual = DateTime.Now;
 
-System.Console.WriteLine(dataAtual);
+int a = 10;
+int b = 5;
 
-DateTime dataAtua2 = DateTime.Now.AddDays(2);
-System.Console.WriteLine(dataAtua2);
-System.Console.WriteLine(dataAtua2.ToString("dd/MM/yyyy HH:mm"));
+int c = a + b;
+
+c +=5;
+
+Console.WriteLine(  c);
+// DateTime dataAtual = DateTime.Now;
+
+// System.Console.WriteLine(dataAtual);
+
+// DateTime dataAtua2 = DateTime.Now.AddDays(2);
+// System.Console.WriteLine(dataAtua2);
+// System.Console.WriteLine(dataAtua2.ToString("dd/MM/yyyy HH:mmgit commit "));
 
 // string apresentacao = "Hello, welcome";
 
