ef22d6617c5a69ce1d8d0feca1907beb49afd3ab
Ramon Pessôa <ramon.palheta.pessoa@hotmail.com>
2022-11-08 15:22:36 -0400

    Criando endpoint para obter resultado por nome

--- CriandoAPI/Controllers/ContatoController.cs
+++ CriandoAPI/Controllers/ContatoController.cs
@@ -41,6 +41,18 @@ namespace CriandoAPI.Controllers
             return Ok(contato);
         }
 
+        [HttpGet("{ObterPorNome}")]
+        public IActionResult ObterPorNome(string nome)
+        {
+            var contatos = _context.Contatos.Where(x=>x.Nome.Contains(nome));
+
+           
+        
+
+            return Ok(contatos);
+        }
+
+
         [HttpPut("{id}")]
         public IActionResult Atualizar(int id,Contato contato)
         {
